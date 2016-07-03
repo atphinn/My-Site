d90abcebb90d94fe5b4cb7499c006f2cb27d5023
Alex Phinn <alexnderphinn@AlexndehinnsMBP.home>
2016-07-03 14:04:05 -0400

    Added my site JS animation

--- mySite/homepage.js
+++ mySite/homepage.js
@@ -1,3 +1,11 @@
+$(document).ready(function(){
+  $('button')
+    .animate({ marginLeft: '600px'},3000)
+    .animate({ marginTop: '-300px', marginLeft: "0"},3000)
+    .animate({ marginTop: "-100",},3000);
+    $("#welcome").fadeIn(9000).delay(10000);
+});
+
 $("button").click(function() {
 	$('#container').fadeIn(4000);
 	$('button').hide()
