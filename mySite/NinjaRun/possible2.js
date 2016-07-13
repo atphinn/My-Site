60dc3a66aa6bd500c553eb4d94dade1b4b450777
Alex <atphinn@gmail.com>
2016-07-12 20:38:55 -0400

    updated site CSS and Javascript

--- mySite/NinjaRun/possible2.js
+++ mySite/NinjaRun/possible2.js
@@ -7,7 +7,7 @@ $(document).keypress(function(e){ x = String.fromCharCode(e.which);
 			 // alert(x);
 			 // console.log(x)
 			if(x == "w"){
-				$('div#box').animate({"top": -200},500).delay(35).animate({"top": 10},1100);
+				$('div#box').animate({"top": -200},500).delay(55).animate({"top": 10},1100);
 }					
 			});
 
@@ -22,6 +22,7 @@ var arrayimg = [ '0', '1', '2', '3'];
 				if (i>= arrayimg.length ){i=0;}
 				$('#ninja').attr('src','run' + arrayimg[i] + '.png');//calls array
 				i++; //cycle to next image;
+				
 			}
 		setInterval(cyclex, 100);
 });
@@ -31,7 +32,7 @@ var arrayimg = [ '0', '1', '2', '3'];
  
  $(document).ready(function(){
  	function ninjaLoop(){
-           $('#obstacle').css({marginLeft: '1620px'}).animate({ marginLeft: '-100px'},3000, ninjaLoop);		
+           $('#obstacle').css({marginLeft: '1720px'}).animate({ marginLeft: '-100px'},3000, ninjaLoop);		
        }
       ninjaLoop();
       // console.log("test")  
@@ -63,7 +64,7 @@ var moveBackground = function(){
 			//Hit detection
 
 			// var playerRightside = document.querySelectorAll("img#ninja");
-			var playerRightside = 200;
+			var playerRightside = 95;
  			var playerBottom = $("#ninja").offset().top;
 			var boxLocation = $("#star").offset().left;
 			 var boxJump = $("#star").offset().top;
