2d0985dda43668c05a2ed2a1aa0c48ac83317333
Alexander Phinn <alexanderphinn@Alexanders-MBP.home>
2017-01-27 21:42:13 -0500

    Added Services, Controller, and view codes

---
+++ Angular/suggestion_box/js/services/suggestions.js
@@ -0,0 +1,29 @@
+app.factory('suggestions', [function () {
+	var demoSuggestions = {
+		posts: [
+			{
+				title: 'Two hour Video Game night',
+				upvotes: 3,
+				comments: [],
+			},
+			{
+				title: 'Pizza and Video Game night',
+				upvotes: 11,
+				comments: [],
+			},
+			{
+				title: 'WWE time',
+				upvotes: 5,
+				comments: [],
+			},
+			{
+				title: 'Prince dance party',
+				upvotes: 12,
+				comments: [],
+			},
+		]
+	};
+
+	return demoSuggestions;
+
+}]);
