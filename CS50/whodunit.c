c94a7893eb0c9b18700e674e35d0056cd2774dee
atphinn <atphinn@gmail.com>
2017-11-26 16:19:08 -0500

    Started Week 4 hw 1

---
+++ CS50/whodunit.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+    if(argc !=2 ){
+        printf(stderr, "Usage: ./whodunit [inputfilename] [outputfilename]\n");
+        return 1;
+
+    }
+}
