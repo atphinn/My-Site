f5faf2968be58b49ff6faf5321ac949371e5ac8a
atphinn <atphinn@gmail.com>
2017-12-06 18:59:07 -0500

    added while loop

--- CS50/Week4/Recover/recover.c
+++ CS50/Week4/Recover/recover.c
@@ -25,16 +25,16 @@ int main(int argc, char *argv[]){
         return 1;
     }
 
-    
-    char* buffer = [512]; 
-    
+
+    char* buffer = malloc[512];
+
     //opens file
     FILE* inptr = fopen(argv[1], "r");
     if (inputr == NULL){
         fprintf(stderr, "Could not open %s.\n", inputr);
         return 2;
     }
-    
+
     //keep count of pictures
     FILE* outptr = fopen(argv[1], "w");
     sprintf(argv[1], "%03i.jpeg", 2);
@@ -46,17 +46,19 @@ int main(int argc, char *argv[]){
 
 //read till buffer of 512 is reached
     fread(buffer, sizeof(512), 1, inptr);
-  
-   
 
-    //find beggining of JPG
-    if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] == 0xf0) == 0xe0 ){
-        fwrite(data,size, number, outptr);
-        //data: Pointer to a struct with byte/ buffer
-    //size: size of each element to read (sizeof) 
-    //number: Number of elements to reaf
-    //inputr: FILE* to read from
-    }
+   while (fread(buffer, sizeof(512), 1, inptr)) {
+     //find beggining of JPG
+     if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] == 0xf0) == 0xe0 ){
+         fwrite(data,size, number, outptr);
+         //data: Pointer to a struct with byte/ buffer
+     //size: size of each element to read (sizeof)
+     //number: Number of elements to reaf
+     //inputr: FILE* to read from
+     }
+   }
+
+
 
 
 }
