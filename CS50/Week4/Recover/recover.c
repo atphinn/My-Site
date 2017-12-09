718aa9f7cc155d1c16b057856f80b5597ef664e6
atphinn <atphinn@gmail.com>
2017-12-08 19:54:48 -0500

    Finished code, but Segmentation fault in final else statment

--- CS50/Week4/Recover/recover.c
+++ CS50/Week4/Recover/recover.c
@@ -13,52 +13,66 @@ repeat untill end of card
 close any remaining files
 
 */
-
-
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char *argv[]){
-    //esures proper usage
-    if (argc != 1){
+//esures proper usage
+    if (argc != 2){
         fprintf(stderr, "Usage: ./recovery [Image file]\n");
         return 1;
     }
 
+    //sets buffer
+    int* buffer = malloc(512);
 
-    char* buffer = malloc[512];
+    //checks for file
+    int jpeg_found = 0;
+    //counter
+    int n = 0;
 
     //opens file
     FILE* inptr = fopen(argv[1], "r");
-    if (inputr == NULL){
-        fprintf(stderr, "Could not open %s.\n", inputr);
+    if (inptr == NULL){
+        fprintf(stderr, "Could not open\n");
         return 2;
     }
+
+    FILE* output == NULL;
 
     //keep count of pictures
-    FILE* outptr = fopen(argv[1], "w");
-    sprintf(argv[1], "%03i.jpeg", 2);
-    if(outptr == NULL){
-        fclose(argv[1]);
-        fprintf(stderr, "Could not find JPEG in %s\n", inputr);
-        return 2;
+
+
+
+    //read till buffer of 512 is reached
+
+
+    while (fread(buffer, sizeof(512), 1, inptr) == 1) {
+    //find beggining of JPG
+        if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0 ){
+            if (jpeg_found == 1){
+
+                fclose(output);
+                char filename[8];
+            sprintf(argv[1], "%03i.jpeg", n);
+            output = fopen(filename, "a");
+            n++;
+
+             fwrite(buffer, sizeof(512),1,output);
+            }else{
+                jpeg_found = 1;
+            }
+
+
+        }
+
     }
 
-//read till buffer of 512 is reached
-    fread(buffer, sizeof(512), 1, inptr);
 
-   while (fread(buffer, sizeof(512), 1, inptr)) {
-     //find beggining of JPG
-     if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] == 0xf0) == 0xe0 ){
-         fwrite(data,size, number, outptr);
-         //data: Pointer to a struct with byte/ buffer
-     //size: size of each element to read (sizeof)
-     //number: Number of elements to reaf
-     //inputr: FILE* to read from
-     }
-   }
+    fclose(inptr);
+    fclose(output);
 
-
-
+    free(buffer);
+    return 0;
 
 }
