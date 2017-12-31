a3c608bb89ae7be7932303a187e01f997834276d
atphinn <atphinn@gmail.com>
2017-12-31 14:47:42 -0500

    Completed first code wars

---
+++ Algo/c/code_kata/doublearithmetic.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+double arithmetic (double a, double b, char* operator);
+
+int main(void){
+
+   arithmetic(1, 2, "multiply");
+}
+
+double arithmetic (double a, double b, char* operator)   {
+  // Your Code here!
+double result;
+  if (strcmp(operator, "add") == 0) {
+
+    result = a+b;
+    printf("The result is %d\n", result);
+  } else if (strcmp(operator ,"subtract") == 0) {
+
+    result = a-b;
+    printf("The result is %d\n", result);
+  } else if (strcmp(operator , "multiply") == 0){
+
+    result = a*b;
+    printf("The result is %d\n", result);
+  }else if (strcmp(operator , "divide") == 0) {
+
+    result = a/b;
+    printf("The result is %d\n", result);
+  }else{
+    return 1;
+  }
+  return 0;
+}
