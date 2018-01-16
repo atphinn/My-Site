b480972a89870f4e90a98eba659b41b121779575
atphinn <atphinn@gmail.com>
2018-01-15 20:01:01 -0500

    Added CS50 week 6 notes

---
+++ CS50/Week6/Week8_notes.py
@@ -0,0 +1,33 @@
+#print("hello, world"); 
+
+#c int main(void){
+#	printf("Hello, WOrld/n")
+#} 
+
+#while True:
+#	print("Hello world")
+
+#c while(true){printf("Hellow, World/n");}
+
+#for i in range(50):
+#	print("Hello, world")
+
+#C for(int i = 0; i <= 50; i++){
+	#printf("Hellow World");
+#}
+
+#import CS50
+#Cs50.get_string()
+#s = input("name: ")
+#print("Hello, world")
+#print("hello, {}".format(s))
+
+
+import cs50
+
+c = cs50.get_char()
+
+if c == "Y" or c == "y":
+ print("yes")
+elif c == "N" or c == "n":
+
