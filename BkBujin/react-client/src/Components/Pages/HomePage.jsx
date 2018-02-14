c8c4ee43eb4221d3f61d6d96d091279b29654a1f
atphinn <atphinn@gmail.com>
2018-02-14 11:41:55 -0500

    added now banner photo

--- BkBujin/react-client/src/Components/Pages/HomePage.jsx
+++ BkBujin/react-client/src/Components/Pages/HomePage.jsx
@@ -6,7 +6,7 @@ class HomePage extends Component {
     return(
     <div id="mainBody">
       <div id="mainBanner">
-      <img src={'https://lh3.googleusercontent.com/cfUGz8_iwIdP01HUxHRkDZDeryH-YYz1h6ZVJ9SRbzHS6-ZkKCywf8g166vC8yYANv7fIQNDT86UYnz36xYMceIoa_PZI7hb0ncQxw4dY_Bvs7lNduBl2NHSlQPT50xjjqALyhBdAm6SukN8Up3_UXSeffbIXI_Y_MSAXfyBT1bfr0gTs2yvrkb_4P3nm1T46Fv9sPRFIZuD32yqcWdCWblkbmYxP86hsQ6g8bLPsBp-pIEWVSuVmP0ttNTITYRfoSqVwvmPHsz09CmP-YShhQlVLy1ZALM9qFzSl7HAWVQ2GGpuaVd7Y8AsWYJDkmVpRuOBbz6aN3-5-39i1OuY0IvfXc0DlLxS_27KPxkkd8xB4_W66AWMT5iHCrOZUM3ge8MbAIwRsg0Og66U4wvyC0vuuFqsIHhy2sUts6yAPIuj5QjJKGC6DNXhuvE_3Sjusg529yYDihQgGMMiOD6VkRxvNo1vxP7VaDWIJkZwbW2JiXk45cPMPgWL21c7LE0Ncchxa3hP_r1yKFxK96G3n4yugNdaGdk1YW9Acu0SUgl88HxafYTfLiQshTnIkFrM-XLBA7J4Y0ZHFwFd0RunaX6Z2ADv_lfwrAICJt6u=w852-h639-no'} alt="jumbotron" className="img-responsive" />
+      <img src={'https://lh3.googleusercontent.com/w3pImt8e0Onm3ukwjrOGTroVKeHokL1YWUHhyXlrgQIk6JV1VxbKg7qOEGnqXD0iDa6HLmE7bZL_liRV294HHe6LWMFLixEVtOHR74sH4edwTM0aTwXix7uotSgUZu04TwjITgl78ZLza2q84T6sh52DLOgcsMy8Mh9UF4mCpHTqXPlsDp-9-GEV5IhdoT_1SNXrsHFkziZjb_QItMY9zbek8sgftdP2KEQmt5XsSwK2itK0Jv7njEYnfi0jH4ahxTSkptMsuA7P8xKOF2Tbr5Yvqao1wz4ZyQgOnIudexw9L8dqtYB1YC7aDH2YexG2lbPyhB4ex4qngSw8SCfxEfSqBRXuNO-KwDRZAFVH6aRkN7IV4-j74bWn9hqQ9i60jHdsCEjyjCb_axL3yw4rOk5ux5d12PeanBLKpYLw0S2l4wugWAxBWZgQvZjBMkGiLVEcwXNrhsESFJ0ghWvuCrR8szgx72eTGnIYUFWeedr44lAN0uP5pPgg6aeOgTllQ1YRkqe3A-oOXcPKAu-zGaToFfFf7Qg3mxbOJ4ePlFrTaSnc6AmUjnayl4d0-DCXQ6F1figAbeAgEKa_iycgVF-UYXECv7_Ha1BLkFGo=w883-h662-no'} alt="jumbotron" className="img-responsive" />
       <div className='typeContainer'>
         <h1>What is BK Muzosa bujinkan?</h1>
         <Link to="/About" className="homePage">Learn More</Link>
