f524fb7c85e4e26b2b2b0285f92366f7c085f287
atphinn <atphinn@gmail.com>
2018-03-06 22:57:26 -0500

    added map component banner, removed styling for update

--- BkBujin/react-client/src/Components/Pages/HomePage.jsx
+++ BkBujin/react-client/src/Components/Pages/HomePage.jsx
@@ -1,22 +1,30 @@
 import React, { PropTypes, Component } from 'react';
 import { Link } from 'react-router-dom';
-import GoogleMap from 'google-map-react';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+import { compose, withProps } from "recompose";
+
+const MyMapComponent = compose(
+  withProps({
+    googleMapURL:  "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `400px` }} />,
+    mapElement: <div style={{ height: `100%` }} />,
+}),
+withScriptjs,
+withGoogleMap
+)
+
+(props => (
+  <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
+    {props.isMarkerShown && (
+      <Marker position={{ lat: -34.397, lng: 150.644 }} />
+    )}
+  </GoogleMap>
+));
+
+
 
 class HomePage extends Component {
-
-  static propTypes () {
-    center: PropTypes.array,
-    zoom: PropTypes.number,
-    greatPlaceCoords: PropTypes.any
-};
-
-static defaultProps = {
-    center: [59.938043, 30.337157],
-    zoom: 9,
-    greatPlaceCoords: {lat: 59.724465, lng: 30.080121}
-  };
-
-
 
   render() {
     return(
@@ -25,18 +33,18 @@ static defaultProps = {
       <img src={'https://lh3.googleusercontent.com/w3pImt8e0Onm3ukwjrOGTroVKeHokL1YWUHhyXlrgQIk6JV1VxbKg7qOEGnqXD0iDa6HLmE7bZL_liRV294HHe6LWMFLixEVtOHR74sH4edwTM0aTwXix7uotSgUZu04TwjITgl78ZLza2q84T6sh52DLOgcsMy8Mh9UF4mCpHTqXPlsDp-9-GEV5IhdoT_1SNXrsHFkziZjb_QItMY9zbek8sgftdP2KEQmt5XsSwK2itK0Jv7njEYnfi0jH4ahxTSkptMsuA7P8xKOF2Tbr5Yvqao1wz4ZyQgOnIudexw9L8dqtYB1YC7aDH2YexG2lbPyhB4ex4qngSw8SCfxEfSqBRXuNO-KwDRZAFVH6aRkN7IV4-j74bWn9hqQ9i60jHdsCEjyjCb_axL3yw4rOk5ux5d12PeanBLKpYLw0S2l4wugWAxBWZgQvZjBMkGiLVEcwXNrhsESFJ0ghWvuCrR8szgx72eTGnIYUFWeedr44lAN0uP5pPgg6aeOgTllQ1YRkqe3A-oOXcPKAu-zGaToFfFf7Qg3mxbOJ4ePlFrTaSnc6AmUjnayl4d0-DCXQ6F1figAbeAgEKa_iycgVF-UYXECv7_Ha1BLkFGo=w883-h662-no'} alt="jumbotron" className="img-responsive" />
       <div className='typeContainer'>
         <h1>What is BK Muzosa bujinkan?</h1>
-        <Link to="/About" className="homePage">Learn More</Link>
+        <Link to="/About" className="homeButton">Learn More</Link>
       </div>
       </div>
       <div id="bujinBanner">
         <img src={'http://www.shingan.ro/wp-content/uploads/2013/04/Masaaki_Hatsumi.jpg'} className="img-responsive" />
         <div className='typeContainer2'>
           <h1>History of the Bujinkan?</h1>
-          <Link to="/About" className="homePage">Learn More</Link>
+          <Link to="/About" className="homeButton">Learn More</Link>
         </div>
       </div>
       <div id="mapBanner">
-
+        <MyMapComponent isMarkerShown/>
       </div>
     </div>
     )
