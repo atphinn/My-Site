018082ad691420dc2fadbb7997a79b7f4edf185f
atphinn <atphinn@gmail.com>
2019-12-04 22:39:42 -0500

    React version of site, Redo the site

---
+++ BKBujin_Gatsby/bkbujin/src/components/header.js
@@ -0,0 +1,59 @@
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+
+const Header = ({ siteTitle }) => (
+  <header
+    style={{
+      background: `black`,
+      marginBottom: `1.45rem`,
+    }}
+  >
+    <div
+      style={{
+        margin: `0 auto`,
+       
+      }}
+    >
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            color: `white`,
+            textDecoration: `none`,
+          }}
+        >
+          {siteTitle}
+        </Link>
+        <Link 
+        to="/page-2/"
+          style={{
+          color: `white`,
+          textDecoration: `none`,
+          }}
+        >
+        About
+        </Link>
+        <Link to="/page-2/"
+        to="/page-2/"
+        style={{
+        color: `white`,
+        textDecoration: `none`,
+        }}
+        >
+          Contact
+          </Link>
+      </h1>
+    </div>
+  </header>
+)
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
