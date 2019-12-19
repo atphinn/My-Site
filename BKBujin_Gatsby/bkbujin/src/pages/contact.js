9da6bc5b617e03d86c7c2c033984894b22c94f7d
atphinn <atphinn@gmail.com>
2019-12-19 13:41:42 -0500

    Add files via upload

--- BKBujin_Gatsby/bkbujin/src/pages/contact.js
+++ BKBujin_Gatsby/bkbujin/src/pages/contact.js
@@ -17,19 +17,19 @@ withGoogleMap
 )
 
 (props => (
-  <GoogleMap defaultZoom={17} defaultCenter={{ lat: 40.666885, lng: -73.973823}}>
+  <GoogleMap defaultZoom={17} defaultCenter={{ lat: 40.678919, lng: -73.982449}}>
     {props.isMarkerShown && (
-      <Marker position={{ lat: 40.666885, lng: -73.973823 }} />
+      <Marker position={{ lat: 40.678919, lng: -73.982449 }} />
+
     )}
   </GoogleMap>
 ));
-
 
 const ContactPage = () => (
   <Layout>
     <SEO title="Contact Page" />
     <div id='map'>
-      <MyMapComponent isMarkerShown/>
+    <MyMapComponent isMarkerShown/>
     </div>
     <div id="contactBody">
         <div id="contactInfo">
